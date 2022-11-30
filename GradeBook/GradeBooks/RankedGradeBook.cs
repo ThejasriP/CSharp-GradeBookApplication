b66139bdc063f83d621d0433ba513a1b9dd80a52
ThejasriP <65957679+ThejasriP@users.noreply.github.com>
2022-11-30 15:34:28 +0530

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,51 +6,50 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook:BaseGradeBook      
     {
-
-        public RankedGradeBook(string name , bool isWeighted) : base(name , isWeighted)
+        public RankedGradeBook(string name, bool val):base(name,val)
         {
-            Type = GradeBookType.Ranked;
+            this.Type  = GradeBookType.Ranked;
         }
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if (Students.Count < 5) throw new InvalidOperationException("Need minimium 5 students ");
 
-            var limit =  (int)Math.Ceiling(Students.Count * 0.2);
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
 
-            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(s => s.AverageGrade)
+                                                    .Select(s=> s.AverageGrade).ToList();
 
-            if (grades[limit - 1] <= averageGrade) return 'A';
-            else if (grades[(limit*2)-1] <= averageGrade) return 'B';
-            else if (grades[(limit*3)-1] <= averageGrade) return 'C';
-            else if (grades[(limit*4)-1] <= averageGrade) return 'D';
-            else return 'F';
-            
-           
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[threshold * 2 - 1] <= averageGrade)
+                return 'B';
+            else if (grades[threshold * 3 - 1] <= averageGrade)
+                return 'C';
+            else if (grades[threshold * 4 - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
+
         }
 
         public override void CalculateStatistics()
         {
-            if (Students.Count < 5) Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
-            else base.CalculateStatistics();
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStatistics();
         }
 
         public override void CalculateStudentStatistics(string name)
         {
             if (Students.Count < 5)
-            {
-                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
-                return;
-            }
-                base.CalculateStudentStatistics(name);
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+            base.CalculateStudentStatistics(name);
         }
-
-
-
-
-
-
     }
 }
