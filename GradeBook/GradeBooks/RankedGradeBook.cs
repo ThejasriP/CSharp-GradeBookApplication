46c4eefb0346fc71f97c1d5c201a02d3ddf36824
ThejasriP <65957679+ThejasriP@users.noreply.github.com>
2022-11-30 15:47:22 +0530

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,55 +1,15 @@
-using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook:BaseGradeBook      
+    class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name, bool val):base(name,val)
+        public RankedGradeBook(string name) : base(name)
         {
-            this.Type  = GradeBookType.Ranked;
-        }
-
-        public override char GetLetterGrade(double averageGrade)
-        {
-
-            if (Students.Count < 5)
-                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
-
-            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
-            var grades = Students.OrderByDescending(s => s.AverageGrade)
-                                                    .Select(s=> s.AverageGrade).ToList();
-
-            if (grades[threshold - 1] <= averageGrade)
-                return 'A';
-            else if (grades[threshold * 2 - 1] <= averageGrade)
-                return 'B';
-            else if (grades[threshold * 3 - 1] <= averageGrade)
-                return 'C';
-            else if (grades[threshold * 4 - 1] <= averageGrade)
-                return 'D';
-            else
-                return 'F';
-
-        }
-
-        public override void CalculateStatistics()
-        {
-            if (Students.Count < 5)
-                Console.WriteLine("Ranked grading requires at least 5 students.");
-            else
-                base.CalculateStatistics();
-        }
-
-        public override void CalculateStudentStatistics(string name)
-        {
-            if (Students.Count < 5)
-                Console.WriteLine("Ranked grading requires at least 5 students.");
-            else
-            base.CalculateStudentStatistics(name);
+            Type = GradeBookType.Ranked;
         }
     }
 }
