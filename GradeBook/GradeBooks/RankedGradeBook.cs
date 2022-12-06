af3d6d27d40c538cc68dcd2772b7adc8cbd7b60c
ThejasriP <65957679+ThejasriP@users.noreply.github.com>
2022-12-06 11:40:20 +0530

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -9,11 +9,58 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class StandardGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name, bool isWeighted) : base(name, isWeighted)
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
-            Type = GradeBookType.Standard;
+            Type = GradeBookType.Ranked;
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            var a = Students.Count / 5;
+            var countHigherGrades = Students.FindAll(x => x.AverageGrade > averageGrade).Count;
+
+            switch (countHigherGrades)
+            {
+                case var b when b < a:
+                    return 'A';
+                case var b when b < 2 * a:
+                    return 'B';
+                case var b when b < 3 * a:
+                    return 'C';
+                case var b when b < 4 * a:
+                    return 'D';
+                default:
+                    return 'F';
+            }
         }
     }
 }
