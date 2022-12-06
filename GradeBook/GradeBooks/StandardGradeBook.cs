aab216cca2ea27cfcba7c7cc955605146d7b14af
ThejasriP <65957679+ThejasriP@users.noreply.github.com>
2022-12-06 11:39:46 +0530

    Update StandardGradeBook.cs

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,16 +1,19 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+
 using GradeBook.Enums;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name) : base(name)
+        public StandardGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
             Type = GradeBookType.Standard;
-
         }
     }
 }
