7727667962b783f76902c81c04947945606be6a7
ThejasriP <65957679+ThejasriP@users.noreply.github.com>
2022-12-06 11:28:54 +0530

    Update StandardGradeBook.cs

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,16 +1,16 @@
-using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-    class StandardGradeBook : BaseGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name, bool isWheighted) : base(name, isWheighted)
+        public StandardGradeBook(string name) : base(name)
         {
-
             Type = GradeBookType.Standard;
+
         }
     }
 }
