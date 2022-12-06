cf99cb5affd23389284760a523104cd186ff4362
ThejasriP <65957679+ThejasriP@users.noreply.github.com>
2022-12-06 11:16:49 +0530

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,17 +1,36 @@
+using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name)
+
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5) throw new InvalidOperationException("Need minimium 5 students ");
+
+            var limit = (int)Math.Ceiling(Students.Count * 0.2);
+
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[limit - 1] <= averageGrade) return 'A';
+            else if (grades[(limit * 2) - 1] <= averageGrade) return 'B';
+            else if (grades[(limit * 3) - 1] <= averageGrade) return 'C';
+            else if (grades[(limit * 4) - 1] <= averageGrade) return 'D';
+            else return 'F';
+
+
+        }
+
         public override void CalculateStatistics()
         {
             if (Students.Count < 5) Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
@@ -27,51 +46,11 @@ namespace GradeBook.GradeBooks
             }
             base.CalculateStudentStatistics(name);
         }
-        public override char GetLetterGrade(double averageGrade)
-        {
-            if (Students.Count < 5)
-            {
-                throw new InvalidOperationException();
-            }
 
 
-            IEnumerable<Student> orderedStudents = Students.OrderByDescending(student => student.AverageGrade);
 
 
-            int idx = 0;
-            foreach (Student student in orderedStudents)
-            {
-                if (averageGrade > student.AverageGrade)
-                {
-                    break;
-                }
 
-                idx++;
-            }
 
-            double percentile = (double)idx / (double)Students.Count * 100;
-
-            if (percentile <= 20.00)
-            {
-                return 'A';
-            }
-            else if (percentile <= 40.00)
-            {
-                return 'B';
-            }
-            else if (percentile <= 60.00)
-            {
-                return 'C';
-            }
-            else if (percentile <= 80.00)
-            {
-                return 'D';
-            }
-            else
-            {
-                return 'F';
-            }
-
-        }
     }
 }
