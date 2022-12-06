4e2be483768a422c9f0cfd0a85f504c1cbbeeb8d
ThejasriP <65957679+ThejasriP@users.noreply.github.com>
2022-12-06 11:08:02 +0530

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GradeBook.Enums;
 
@@ -10,6 +11,52 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+
+            IEnumerable<Student> orderedStudents = Students.OrderByDescending(student => student.AverageGrade);
+
+
+            int idx = 0;
+            foreach (Student student in orderedStudents)
+            {
+                if (averageGrade > student.AverageGrade)
+                {
+                    break;
+                }
+
+                idx++;
+            }
+
+            double percentile = (double)idx / (double)Students.Count * 100;
+
+            if (percentile <= 20.00)
+            {
+                return 'A';
+            }
+            else if (percentile <= 40.00)
+            {
+                return 'B';
+            }
+            else if (percentile <= 60.00)
+            {
+                return 'C';
+            }
+            else if (percentile <= 80.00)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
         }
     }
 }
